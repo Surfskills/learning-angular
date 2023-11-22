c839e0185ad5a07e1dc418802cff68d259a6fa69
Surfskills <ojjfred@gmail.com>
2023-11-22 09:28:09 +0000

    Learning

--- inmest-web/src/app/app.component.ts
+++ inmest-web/src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule, } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnChanges{
   title = 'inmest-web';
+  name = "Fred";
+  profile = {
+    id:1,
+    first_name: "Fred",
+    last_name: "Osege"
+  }
+  constructor(){
+    console.log("constructor")
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    for(const inputChanges in changes){
+      console.log(changes[inputChanges], inputChanges);
+
+    }
+    
+  }
 }
